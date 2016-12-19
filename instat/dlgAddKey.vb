5ad69aec776f0d32589014f5c800064da48141cb
dawarlord <kmanjang@pristineconsulting.com>
2016-12-19 12:02:36 +0000

    Created a dialog for adding keys

---
+++ instat/dlgAddKey.vb
@@ -0,0 +1,26 @@
+﻿Public Class dlgAddKey
+
+    Private Sub UcrReceiverExpression1_Load(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Private Sub UcrReceiverMultiple1_Load(sender As Object, e As EventArgs) Handles ucrReceiverKeyColumns.Load
+
+    End Sub
+
+    Private Sub dlgAddKey_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblKeyName.Click
+
+    End Sub
+
+    Private Sub UcrInputTextBox1_Load(sender As Object, e As EventArgs) Handles ucrInputKeyName.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles cmdCheckUnique.Click
+
+    End Sub
+End Class
